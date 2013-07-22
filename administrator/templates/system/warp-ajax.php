3917b5a78ae1da1f1d349eea09c375aff1c199ea
greendezine <scott@greendezine.com>
2013-07-22 22:31:58 +0900

    replace jquery.placeholder.js

--- administrator/templates/system/warp-ajax.php
+++ administrator/templates/system/warp-ajax.php
@@ -11,9 +11,10 @@ defined('_JEXEC') or die('Restricted access');
 
 // get template data/path
 $data   = JFactory::getApplication()->input->get('jform', array(), 'array');
-$config = JPATH_ROOT."/templates/{$data['template']}/config.php";
+$templ  = isset($data['template']) ? $data['template'] : '';
+$config = JPATH_ROOT."/templates/{$templ}/config.php";
 
-if (file_exists($config)) {
+if ($templ && file_exists($config)) {
 
 	// load template config
 	require_once($config);
